fa163aebbb20d8cef1f8cff65059e0cc056f6483
rupesh-brs <alajangirupesh@gmail.com>
2024-10-28 10:40:38 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./modules/Home";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+    </>
+  );
+}
+
+export default App;
